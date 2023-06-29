42351fe5d4908f650ecf180f64e40506f4a7325e
ahatem20 <a.hatem20@live.com>
2023-06-29 15:01:12 +0300

    change files attrib

--- ioUtils/ioUtilities.py
+++ ioUtils/ioUtilities.py
@@ -1,18 +1,34 @@
 import os
+import ctypes
 
 
-def writetoFile(dataBytes, fileName):
-    path = 'data/{}'.format(fileName)
-    with open(path, "wb") as bFile:
-        bFile.write(dataBytes)
+def writetoFile(dataBytes, fileName, currentOS):
+    if currentOS == 'Windows':
+        path = 'data/{}'.format(fileName)
+        with open(path, "wb") as bFile:
+            bFile.write(dataBytes)
+        ctypes.windll.kernel32.SetFileAttributesW(path, 0x02)
+    elif currentOS == 'Linux':
+        path = '.data/.{}'.format(fileName)
+        with open(path, "wb") as bFile:
+            bFile.write(dataBytes)
 
 
-def readfromFile(fileName):
-    path = 'data/{}'.format(fileName)
+def readfromFile(fileName, currentOS):
+    if currentOS == 'Windows':
+        path = 'data/{}'.format(fileName)
+    elif currentOS == 'Linux':
+        path = '.data/.{}'.format(fileName)
+
     with open(path, "rb") as bFile:
         dataBytes = bFile.read()
     return dataBytes
 
 
-def deleteFile(fileName):
-    os.remove('data/{}'.format(fileName))
+def deleteFile(fileName, currentOS):
+    if currentOS == 'Windows':
+        path = 'data/{}'.format(fileName)
+    elif currentOS == 'Linux':
+        path = '.data/.{}'.format(fileName)
+
+    os.remove(path)
