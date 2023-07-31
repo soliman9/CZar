87af186a50585f79dca28fa13b4dcda719642b77
ahatem20 <a.hatem20@live.com>
2023-07-31 21:15:29 +0300

    key gen functions

--- crypto/aes.py
+++ crypto/aes.py
@@ -1,12 +1,14 @@
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-from os import urandom
+from secrets import token_bytes
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
 def encrypt(data, aad, key=None, nonce=None):
     if key is None:
         key = AESGCM.generate_key(bit_length=256)
     if nonce is None:
-        nonce = urandom(12)
+        nonce = token_bytes(12)
     aesgcm = AESGCM(key)
     return (aesgcm.encrypt(nonce, data, aad),
             key,
@@ -17,3 +19,18 @@ def encrypt(data, aad, key=None, nonce=None):
 def decrypt(key, cData, aad, nonce):
     aesgcm = AESGCM(key)
     return aesgcm.decrypt(nonce, cData, aad)
+
+
+def generateSalt():
+    return token_bytes(16)
+
+
+def generateKey(salt, password):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=480000
+    )
+    key = kdf.derive(password)
+    return key
